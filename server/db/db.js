5488ddb965a1e1547d9e4895f0e93816188d9169
Ryo Wheatley <ryo.capricorn92@gmail.com>
2016-10-04 13:44:29 -0400

    (feat) add mongoose skeleton

--- server/db/db.js
+++ server/db/db.js
@@ -1,1 +1,12 @@
-//
+var mongoose = require('mongoose');
+
+var LinkSchema = new mongoose.Schema({
+  visits: Number,
+  link: String,
+  title: String,
+  code: String,
+  base_url: String,
+  url: String
+});
+
+module.exports = mongoose.model('Link', LinkSchema);
