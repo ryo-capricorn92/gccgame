7877f4f022ed9ba795036601ca72be6dbfd1fb74
Ryo Wheatley <ryo.capricorn92@gmail.com>
2016-11-10 15:54:08 -0800

    (fix) console testing

--- server.js
+++ server.js
@@ -13,8 +13,10 @@ app.set('view engine', 'ejs');
 require('./server/routes.js')(app, express);
 
 app.get('/', function(req, res) {
+  console.log('GOT TO THE / ROUTE');
   res.render('index');
 });
+
 var port = process.env.PORT || 3000;
 
 db.sync().then(function () {
